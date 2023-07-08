2e626c17c4337ca3b4fc8074f98388003559d147
Gautham Vanjre <vanjregautham@gmail.com>
2023-07-08 19:34:14 +0530

    updated env variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,25 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth } from "@firebase/auth";
-import {
-  API_KEY,
-  APP_ID,
-  AUTH_DOMAIN,
-  MESSAGING_SENDER,
-  PROJECT_ID,
-  STORAGE_BUCKET,
-} from "./firebase-config";
+// import {
+//   API_KEY,
+//   APP_ID,
+//   AUTH_DOMAIN,
+//   MESSAGING_SENDER,
+//   PROJECT_ID,
+//   STORAGE_BUCKET,
+// } from "./firebase-config";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER,
-  appId: APP_ID,
+  apiKey: "AIzaSyB-TcXldsexDTGH-0LpdQKE5Y63xG7GlMQ",
+  authDomain: "eventconnect-bd75a.firebaseapp.com",
+  projectId: "eventconnect-bd75a",
+  storageBucket: "eventconnect-bd75a.appspot.com",
+  messagingSenderId: "820272170787",
+  appId: "1:820272170787:web:d8780303f6f5b99bb7ee96",
 };
 
 // Initialize Firebase
